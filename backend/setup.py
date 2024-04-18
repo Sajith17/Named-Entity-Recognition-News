0079ab3b32ad740d99d74ca79ead39d1c31e0edd
Sajith17 <sajithseelan17@gmail.com>
2024-04-18 20:01:13 +0530

    setup packages

---
+++ backend/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Named-Entity-Recognition-News"
+AUTHOR_USER_NAME = "Sajith17"
+SRC_REPO = "NamedEntityRecognition"
+AUTHOR_EMAIL = "sajithseelan17@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for NER app",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="src")
+)
