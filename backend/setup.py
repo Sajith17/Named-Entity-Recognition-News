b0f65ce0c7a1bd1001b49d54a264fb4b392895b1
Sajith17 <sajithseelan17@gmail.com>
2024-04-18 20:39:19 +0530

    package setup

--- backend/setup.py
+++ backend/setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"": "."},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
--- backend/template.py
+++ backend/template.py
@@ -24,7 +24,6 @@ list_of_files = [
     "app.py",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
 ]
 
 for filepath in list_of_files:
@@ -35,7 +34,7 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory: {filedir} for the file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath==0)):
         with open(filepath, 'w') as f:
             pass
         logging.info(f"Creating empty file: {filepath}")
