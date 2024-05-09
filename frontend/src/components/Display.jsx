0cca567ded09f7fddb19c909aa3e82e5e5121f22
Sajith17 <sajithseelan17@gmail.com>
2024-05-09 09:09:02 +0530

    screenshot added

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -2,7 +2,7 @@ import Tag from "./Tag";
 
 function Display({ entities, tagColor, selectedLabels }) {
   return (
-    <div className="pl-10 pr-10 mt-8 leading-7 max-w-[700px]">
+    <div className="pl-10 pr-10 mt-8 leading-8 max-w-[700px]">
       {entities.map((item, index) => {
         if (item[1] && selectedLabels[item[1]])
           return (
